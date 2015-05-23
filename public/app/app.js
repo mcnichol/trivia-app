f4e1b1cbee8cdc36d05c694f094962c98c0cc8ca
Michael <mcnichol.m@gmail.com>
2015-05-23 17:43:55 -0500

    Refactor app.js

--- public/app/app.js
+++ public/app/app.js
@@ -1,8 +1,24 @@
 angular.module('app', ['ngResource', 'ngRoute']);
 
 angular.module('app').config(function($routeProvider, $locationProvider){
-   $locationProvider.html5Mode(true);
-   $routeProvider
-      .when('/', {templateUrl: '/partials/main/main', controller: 'mvMainCtrl'})
-      .when('/admin/users', {templateUrl: '/partials/admin/user-list', controller: 'mvUserListCtrl'})
+  var routeRoleChecks = {
+    admin: {auth: function(mvAuth){
+      return mvAuth.authorizeCurrentUserForRoute('admin')
+    }}
+  }
+
+  $locationProvider.html5Mode(true);
+  $routeProvider
+    .when('/', {templateUrl: '/partials/main/main', controller: 'mvMainCtrl'})
+    .when('/admin/users', {templateUrl: '/partials/admin/user-list', 
+      controller: 'mvUserListCtrl', resolve: routeRoleChecks.admin
+    })
 });
+
+angular.module('app').run(function($rootScope, $location){
+  $rootScope.$on('$routeChangeError', function(evt, current, previous, rejection){
+    if(rejection === 'Not Authorized'){
+      $location.path('/');
+    }
+  })
+})
