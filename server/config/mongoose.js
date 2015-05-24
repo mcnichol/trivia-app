aa395d45e1e86b249739f7455e1d408804ffa4d8
Michael <mcnichol.m@gmail.com>
2015-05-24 09:10:24 -0500

    Signup Integration

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,8 +1,7 @@
 var mongoose 	= require('mongoose'),
-    crypto	= require('crypto');
+    userModel  	= require('../models/User');
  
 module.exports = function(config){
-
   mongoose.connect(config.db);
   var db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error...'));
@@ -10,44 +9,7 @@ module.exports = function(config){
      console.log(config.msg);
   });
 
-  var userSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    username: String,
-    salt: String,
-    hashed_pwd: String,
-    roles: [String]
-    
-  });
-  userSchema.methods={
-    authenticate: function(passwordToMatch){
-      return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
-    }
-  }
-  var User = mongoose.model('User', userSchema);
-
-  User.find({}).exec(function(err, collection){
-    if(collection.length === 0){
-      var salt, hash;
-      salt = createSalt();
-      hash = hashPwd(salt, 'mmcnk'); 
-      User.create({firstName:'Michael',lastName:'McNichol',username:'mmcnk', salt: salt, hashed_pwd: hash, roles: ['admin']});
-      
-      salt = createSalt();
-      hash = hashPwd(salt, 'augs'); 
-      User.create({firstName:'August',lastName:'McNichol',username:'augs', salt:salt, hashed_pwd:hash, roles: []});
-      console.log("Users Added");
-    }else{
-      console.log("Users have been found in DB");
-    }
-  });
+  userModel.createDefaultUsers();
 }
 
-function createSalt(){
-  return crypto.randomBytes(128).toString('base64');
-}
 
-function hashPwd(salt, pwd){
-  hmac = crypto.createHmac('sha1',salt);
-  return hmac.update(pwd).digest('hex');
-}
