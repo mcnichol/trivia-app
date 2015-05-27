12f467f40cc2fb624ef2866c766da371baec971d
Michael <mcnichol.m@gmail.com>
2015-05-26 22:33:18 -0500

    Adding Events

--- public/app/app.js
+++ public/app/app.js
@@ -1,44 +1,46 @@
 angular.module('app', ['ngResource', 'ngRoute']);
 
 angular.module('app').config(function($routeProvider, $locationProvider){
-  var routeRoleChecks = {
-    admin: {auth: function(mvAuth){
-      //console.log("Entering Route Role Check with Admin Role")
-      return mvAuth.authorizeCurrentUserForRoute('admin');
-      //This is unreachable
-    }},
-    user: {auth: function(mvAuth){
-      //console.log("Enter Route Role Checks User Check");
-      return mvAuth.authorizeAuthenticatedUserForRoute();
-      //This is unreachable
-    }}
-  }
+    var routeRoleChecks = {
+        admin: {auth: function(mvAuth){
+        //console.log("Entering Route Role Check with Admin Role")
+        return mvAuth.authorizeCurrentUserForRoute('admin');
+        //This is unreachable
+        }},
+        user: {auth: function(mvAuth){
+        //console.log("Enter Route Role Checks User Check");
+        return mvAuth.authorizeAuthenticatedUserForRoute();
+        //This is unreachable
+        }}
+    };
 
-  $locationProvider.html5Mode(true);
-  $routeProvider
-    .when('/', {templateUrl: '/partials/main/main', controller: 'mvMainCtrl'
+    $locationProvider.html5Mode(true);
+    $routeProvider
+    .when('/', {templateUrl: '/partials/main/main', controller: 'mvMainCtrl'})
+    .when('/admin/users', {templateUrl: '/partials/admin/user-list',
+        controller: 'mvUserListCtrl', resolve: routeRoleChecks.admin
     })
-    .when('/admin/users', {templateUrl: '/partials/admin/user-list', 
-      controller: 'mvUserListCtrl', resolve: routeRoleChecks.admin
+    .when('/signup', {templateUrl: '/partials/account/signup',
+        controller: 'mvSignupCtrl'
     })
-    .when('/signup', {templateUrl: '/partials/account/signup', 
-      controller: 'mvSignupCtrl'
+    .when('/profile', {templateUrl: '/partials/account/profile',
+        controller: 'mvProfileCtrl', resolve: routeRoleChecks.user
     })
-    .when('/profile', {templateUrl: '/partials/account/profile', 
-      controller: 'mvProfileCtrl', resolve: routeRoleChecks.user
+    .when('/events', {templateUrl: '/partials/events/event-list',
+        controller: 'mvEventListCtrl'
     })
-    .when('/categories', {templateUrl: '/partials/categories/category-list', 
-      controller: 'mvCategoryListCtrl'
+    .when('/categories', {templateUrl: '/partials/categories/category-list',
+        controller: 'mvCategoryListCtrl'
     })
-    .when('/categories/:id', {templateUrl: '/partials/categories/category-details', 
-      controller: 'mvCategoryDetailCtrl'
+    .when('/categories/:id', {templateUrl: '/partials/categories/category-details',
+        controller: 'mvCategoryDetailCtrl'
     })
 });
 
 angular.module('app').run(function($rootScope, $location){
-  $rootScope.$on('$routeChangeError', function(evt, current, previous, rejection){
-    if(rejection === 'Not Authorized'){
-      $location.path('/');
-    }
-  })
-})
+    $rootScope.$on('$routeChangeError', function(evt, current, previous, rejection){
+        if(rejection === 'Not Authorized') {
+            $location.path('/');
+        }
+    })
+});
